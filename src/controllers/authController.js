58855a8c6567badca39d17200ec2017cecdb2675
Vinipereir <158165909+Vinipereir@users.noreply.github.com>
2025-05-13 09:33:53 -0300

    rafactor: add find user by nickname verification controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,20 +17,28 @@ class AuthController {
   // Registrar novo usuário
   async register(req, res) {
     try {
-      const { name, email, password } = req.body;
+      const { name, nickname, email, password } = req.body;
 
       // Validação básica
-      if (!name || !email || !password) {
+      if (!name || !nickname  || !email || !password ) {
         return res
           .status(400)
           .json({ error: "Os campos nome, email e senha são obrigatórios!" });
       }
 
       // Verificar se o usuário já existe
-      const userExists = await UserModel.findByEmail(email);
-      if (userExists) {
+      const userEmailExists = await UserModel.findByEmail(email);
+      if (userEmailExists) {
         return res.status(400).json({ error: "Este email já está em uso!" });
       }
+
+
+     /* const userNicknameExists = await UserModel.findByEmail(nickname);
+      if (userNicknameExists) {
+        return res.status(400).json({ error: "Este Nickname já está em uso!" });
+      }
+        */
+
 
       // Hash da senha
       const hashedPassword = await bcrypt.hash(password, 10);
@@ -38,6 +46,7 @@ class AuthController {
       // Criar objeto do usuário
       const data = {
         name,
+        nickname,
         email,
         password: hashedPassword,
       };
@@ -86,7 +95,9 @@ class AuthController {
         {
           id: userExists.id,
           name: userExists.name,
+          nickname: userExists.nickname,
           email: userExists.email,
+         
         },
         process.env.JWT_SECRET,
         {
